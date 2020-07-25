4bfc548e518c3897e9517e9952c4f0934385ffab
5000 <63864045+5O00@users.noreply.github.com>
2020-07-25 15:54:58 +0200

    Delete ServPort.py

--- ServPort.py
+++
@@ -1,5 +0,0 @@
-import socket
-
-def ServerOnPort(Number_Port, Protocol):
-    ServiceName = socket.getservbyport(Number_Port, Protocol)
-    print("[+] port number %d : %s"%(Number_Port, ServiceName))
